c68791ddacb27626b72231132a636cbaf1343457
Eduardo Araújo <edu.araujo94@live.com>
2018-06-10 20:04:40 -0300

    main
    Codigos Pong

---
+++ paddle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class paddle : MonoBehaviour {
+	public float speed = 30;
+	public string axis = "Vertical";
+	public int pointsLeft = 0;
+	public int pointsRight = 0;
+	// Update is called once per frame
+	void FixedUpdate () {
+		float v = Input.GetAxisRaw (axis);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0,v) * speed;
+	}
+}
---
+++ wall.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class wall : MonoBehaviour {
+	static private int pointsLeft;
+	static private int pointsRight;
+
+	public Text left;
+	public Text right;
+	// Use this for initialization
+	void Start () {
+		left.text = "";
+		right.text = "";
+
+		left.text = pointsLeft.ToString();
+		right.text = pointsRight.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			
+		}
+
+	}
+
+	void OnTriggerEnter2D() {
+		if(this.tag == "left" ) {
+			pointsRight++;
+			Debug.Log("Right" +pointsRight);
+			SceneManager.LoadScene(SceneManager.GetActiveScene ().name);
+
+		} else {
+			pointsLeft++;
+			Debug.Log("left" +pointsLeft);
+			SceneManager.LoadScene(SceneManager.GetActiveScene ().name);
+		}
+
+	}
+}
